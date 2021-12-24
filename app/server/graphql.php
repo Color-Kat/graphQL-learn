deb1ec5af5c7796691f53e9b5240e77a0bb55782
Color-Kat <katik7979@gmail.com>
2021-12-24 20:56:44 +0300

    graphQL server

---
+++ app/server/graphql.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+use GraphQL\GraphQL;
+use GraphQL\Type\Schema;
+
+$queryType = new ObjectType([
+    'name' => 'Query',
+    'fields' => [
+        'echo' => [
+            'type' => Type::string(),
+            'args' => [
+                'message' => Type::nonNull(Type::string()),
+            ],
+            'resolve' => function ($rootValue, $args) {
+                return $rootValue['prefix'] . $args['message'];
+            }
+        ],
+    ],
+]);
+
+
+$schema = new Schema([
+    'query' => $queryType
+]);
+
+$rawInput = file_get_contents('php://input');
+$input = json_decode($rawInput, true);
+$query = $input['query'];
+$variableValues = $input['variables'] ?? [];
+
+try {
+    $rootValue = ['prefix' => 'You said: '];
+    $result = GraphQL::executeQuery($schema, $query, $rootValue, null, $variableValues);
+    $output = $result->toArray();
+} catch (\Exception $e) {
+    $output = [
+        'errors' => [
+            [
+                'message' => $e->getMessage()
+            ]
+        ]
+    ];
+}
+header('Content-Type: application/json');
+echo json_encode($output);
