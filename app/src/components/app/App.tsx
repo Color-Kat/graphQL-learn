858e8feb1c279c1f8aa63d7ed9feadcf0073cdca
Color-Kat <katik7979@gmail.com>
2022-01-01 21:26:07 +0300

    useState

--- app/src/components/app/App.tsx
+++ app/src/components/app/App.tsx
@@ -3,15 +3,30 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    const [count, setCount] = useState(0);
+    const [activity, setActivity] = useState({
+        activity: 'food',
+        currency: 'RUB'
+    });
 
+    function increment() {
+        setCount(prev => prev+1);
+    }
+    function decrement() { setCount(prev => prev - 1); }
+    
+    function changeActivity(e: any) {
+        setActivity(prev => ({...prev, activity: e.target.value}));
+    }
 
     return (
         <div className="App">
             <div>
-                <h1 className="">Counter: <b></b></h1>
+                <h1>Accounting:</h1>
+                <input onChange={changeActivity} />
+                <h1 className="color">{activity.activity}: <b>{count}{ activity.currency }</b></h1>
 
-                <button>Increase</button>
-                <button>Decrease</button>
+                <button onClick={increment}>Increase</button>
+                <button onClick={decrement}>Decrease</button>
             </div>
         </div>
     );
