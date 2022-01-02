a9290108bcb00d829fe067dcedcf913b5ea1272f
Color-Kat <katik7979@gmail.com>
2022-01-02 15:23:30 +0300

    useEffect

--- app/src/components/app/App.tsx
+++ app/src/components/app/App.tsx
@@ -1,32 +1,61 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './App.css';
 
-function App() {
-    const [count, setCount] = useState(0);
-    const [activity, setActivity] = useState({
-        activity: 'food',
-        currency: 'RUB'
-    });
+// useState
+// function App() {
+//     const [count, setCount] = useState(0);
+//     const [activity, setActivity] = useState({
+//         activity: 'food',
+//         currency: 'RUB'
+//     });
 
-    function increment() {
-        setCount(prev => prev+1);
-    }
-    function decrement() { setCount(prev => prev - 1); }
+//     function increment() {
+//         setCount(prev => prev+1);
+//     }
+//     function decrement() { setCount(prev => prev - 1); }
     
-    function changeActivity(e: any) {
-        setActivity(prev => ({...prev, activity: e.target.value}));
-    }
+//     function changeActivity(e: any) {
+//         setActivity(prev => ({...prev, activity: e.target.value}));
+//     }
+
+//     return (
+//         <div className="App">
+//             <div>
+//                 <h1>Accounting:</h1>
+//                 <input onChange={changeActivity} />
+//                 <h1 className="color">{activity.activity}: <b>{count}{ activity.currency }</b></h1>
+
+//                 <button onClick={increment}>Increase</button>
+//                 <button onClick={decrement}>Decrease</button>
+//             </div>
+//         </div>
+//     );
+// }
+
+// useEffect
+function App() {
+    const [page, setPage] = useState('posts');
+    const [content, setContent] = useState('Loading');
+    
+    useEffect(() => {
+        setContent('Loading');
+        fetch(`https://jsonplaceholder.typicode.com/${page}/1`)
+            .then(response => response.json())
+            .then(json => setContent(JSON.stringify(json, null, 2))
+            )
+                    
+    }, [page]);
 
     return (
         <div className="App">
             <div>
-                <h1>Accounting:</h1>
-                <input onChange={changeActivity} />
-                <h1 className="color">{activity.activity}: <b>{count}{ activity.currency }</b></h1>
-
-                <button onClick={increment}>Increase</button>
-                <button onClick={decrement}>Decrease</button>
+                <h1>{page}</h1>
+                <button onClick={()=>setPage('posts')}>Posts</button>
+                <button onClick={() => setPage('photos')}>photos</button>
+                <button onClick={() => setPage('users')}>users</button>
+                <br />
+                <span ><pre>{content}</pre></span>
             </div>
         </div>
     );
