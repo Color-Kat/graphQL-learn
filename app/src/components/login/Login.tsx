30325f41c4f4ab3d469d398d09033a7ac4faeecc
Color-Kat <katik7979@gmail.com>
2022-01-07 20:19:55 +0300

    login form

--- app/src/components/login/Login.tsx
+++ app/src/components/login/Login.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+interface ILoginUser {
+  name: string;
+  password: string;
+}
 
 export const Login: React.FC = () => {
-  const loginHandler = () => {
-    console.log("logining...");
+  const [loginData, setLoginData] = useState<ILoginUser>({
+    name: "",
+    password: "",
+  });
+
+  const error: string = "";
+
+  function nameHandler(event: React.ChangeEvent<HTMLInputElement>): void {
+    setLoginData((prev) => ({ ...prev, name: event.target.value }));
+  }
+
+  function passwordHandler(event: React.ChangeEvent<HTMLInputElement>): void {
+    setLoginData((prev) => ({ ...prev, password: event.target.value }));
+  }
+
+  const loginHandler = (e: React.FormEvent) => {
+    e.preventDefault();
   };
 
   return (
@@ -13,7 +33,10 @@ export const Login: React.FC = () => {
 
       <h3 className="text-3xl my-4 text-gray-700">Login</h3>
 
-      <form onSubmit={loginHandler} className="rounded px-8 pt-6 pb-8 mb-4">
+      <form
+        onSubmit={loginHandler}
+        className="rounded px-8 pt-6 pb-8 mb-4 mx-auto md:max-w-lg"
+      >
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
@@ -25,6 +48,8 @@ export const Login: React.FC = () => {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="username"
             type="text"
+            onChange={nameHandler}
+            value={loginData.name}
             placeholder="Username"
           />
         </div>
@@ -36,19 +61,19 @@ export const Login: React.FC = () => {
             Password
           </label>
           <input
-            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
-            placeholder="******************"
+            onChange={passwordHandler}
+            value={loginData.password}
+            placeholder="***************"
           />
-          <p className="text-red-500 text-xs italic">
-            Please choose a password.
-          </p>
+          <p className="text-red-500 text-xs italic">{error}</p>
         </div>
         <div className="flex items-center justify-between">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+            type="submit"
           >
             Sign In
           </button>
